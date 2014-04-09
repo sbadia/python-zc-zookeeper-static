316d7b9858dc426aae11dc56525c5cae1b5eb88a
Sebastien Badia <seb@sebian.fr>
2014-04-09 22:44:10 +0200

    Imported Upstream version 3.4.4

---
+++ get_source_files.py
@@ -0,0 +1,57 @@
+import os
+import shutil
+import sys
+
+
+def main():
+    here = os.getcwd()
+
+    [tarball] = sys.argv[1:]
+    use_patch = '3.4.4' in tarball
+    os.system('rm -rf zookeeper-sources')
+    os.mkdir('zookeeper-sources')
+    os.chdir('zookeeper-sources')
+    os.system('tar xzf ' + tarball)
+    [srcdir] = os.listdir('.')
+
+    if os.path.exists(os.path.join(here, 'src')):
+        shutil.rmtree(os.path.join(here, 'src'))
+    os.mkdir(os.path.join(here, 'src'))
+    os.chdir(os.path.join(srcdir, 'src'))
+    os.system("tar czf %s c" % os.path.join(here, 'c.tgz'))
+    shutil.copy(os.path.join('c', 'LICENSE'), here)
+    os.chdir(os.path.join('contrib', 'zkpython'))
+    shutil.copy('README', os.path.join(here, 'ORIGINAL-README'))
+    os.chdir(os.path.join('src'))
+
+    if use_patch:
+        shutil.copy(os.path.join(here, 'zookeeper_patched_344.c'),
+                    os.path.join('c', 'zookeeper.c'))
+
+    shutil.copytree('test', os.path.join(here, 'src', 'zookeepertests'))
+    open(os.path.join(here, 'src', 'zookeepertests', '__init__.py'),
+         'w').close()
+    connection_test = open(
+        os.path.join(here, 'src', 'zookeepertests', 'connection_test.py')
+        ).read()
+    f = open(
+        os.path.join(here, 'src', 'zookeepertests', 'connection_test.py'),
+        'w')
+    if ('handles = [ zookeeper.init(self.host) for i in xrange(63) ]'
+        in connection_test):
+        connection_test = connection_test.replace(
+            'handles = [ zookeeper.init(self.host) for i in xrange(63) ]',
+            'handles = [ zookeeper.init(self.host) for i in xrange(9) ]')
+    else:
+        connection_test = connection_test.replace('testmanyhandles',
+                                                  'disabledtestmanyhandles')
+
+    f.write(connection_test)
+    f.close()
+    os.chdir(os.path.join('c'))
+    shutil.copy('pyzk_docstrings.h', here)
+    shutil.copy('zookeeper.c', here)
+    os.chdir(here)
+
+if __name__ == '__main__':
+    main()
---
+++ setup.py
@@ -0,0 +1,101 @@
+#  Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+version = '3.4.4'
+
+import distutils.command.build_ext
+import distutils.core
+import os
+import shutil
+import time
+
+
+def do_system(cmd):
+    print cmd
+    if os.system(cmd):
+        raise SystemError("Failed: %s" % cmd)
+
+
+class build_ext(distutils.command.build_ext.build_ext):
+
+    def run(self):
+        # Hack to build C sources first
+        if os.path.exists('c'):
+            print "Removing old c directory"
+            shutil.rmtree('c')
+        do_system("tar xzf c.tgz")
+        os.utime("c/config.h.in", (time.time(), time.time()))
+        do_system("cd c; ./configure; make")
+
+        distutils.command.build_ext.build_ext.run(self)
+
+cmdclass = dict(build_ext=build_ext)
+
+try:
+    import setuptools
+except ImportError:
+    from distutils.core import setup
+else:
+    from setuptools import setup
+
+    import setuptools.command.test
+
+    class Test(setuptools.command.test.test):
+        def run(self):
+            import socket
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.settimeout(1.0)
+            ruok = None
+            try:
+                s.connect(('localhost', 22182))
+                s.send('ruok')
+                ruok = s.recv(4)
+            except socket.error:
+                pass
+            if ruok != 'imok':
+                raise SystemError(
+                    'You must be running a testing ZooKeeper server '
+                    'on port 22182')
+
+            os.environ["ZKPY_LOG_DIR"] = '.'
+            setuptools.command.test.test.run(self)
+
+        import sys
+        sys.path.append('src')
+
+    cmdclass.update(test=Test)
+
+setup(
+    author='Henry Robinson',
+    author_email='henry@cloudera.com',
+    license='Apache',
+    name='zc-zookeeper-static',
+    version=version,
+    long_description=open('README.rst').read(),
+    description='ZooKeeper Python bindings',
+    url='http://pypi.python.org/pypi/zc-zookeeper-static',
+    cmdclass=cmdclass,
+    test_suite='zookeepertests',
+    ext_modules=[
+        distutils.core.Extension(
+            "zookeeper",
+            sources=["zookeeper.c"],
+            include_dirs=["c/include",
+                          "c/generated"],
+            extra_objects=["c/.libs/libzookeeper_mt.a"],
+        )
+    ],
+)
